95064d93a03d187969a6a97ee6fc44ebfa216aa1
EkaterinaTokar <ekaterinatokar19@gmail.com>
2023-06-21 12:52:54 +0500

    feat:addaed apiKey

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd4726672232f4e6ab04ee17d743a48bb', // получите свой ключ https://newsapi.org/
         });
     }
 }
